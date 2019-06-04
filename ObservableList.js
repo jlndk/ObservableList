d4fa56f750a1da0563ef738e133e8068e1ddc435
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2019-06-04 12:30:35 +0200

    Initial version of Readme and Code

---
+++ ObservableList.js
@@ -0,0 +1,123 @@
+export class ObservableList extends Array {
+    /**
+     * Create a new ObservableList.
+     *
+     * @param {Iterable|undefined} value a iterable collection of values the list should be initialized with.
+     */
+    constructor(value) {
+        if(Symbol.iterator in Object(value)){
+            super(...value);
+        } else {
+            super();
+        }
+        this.subscribers = [];
+    }
+
+    /**
+     * Register a new subscriber for this list.
+     * Each time the list is updated it will be notified via the supplied callback.
+     *
+     * @return {Function} A "unsubscribe" function which will undo the subscription once called.
+     */
+    subscribe(handler, invalidate = function(){}) {
+        const subscriber = {handler, invalidate};
+        this.subscribers.push(subscriber);
+
+        handler(this);
+        return () => {
+            const i = this.subscribers.indexOf(subscriber);
+            if (i !== -1) {
+                this.subscribers.splice(i, 1);
+            }
+        };
+    }
+
+    /**
+     * Replaces all items in this list with the content of a given iterable collection of other items.
+     *
+     * @param  {Iterable} items An iterable collection of the items which this list should contain.
+     */
+    replaceAll(items) {
+        this.splice(0, items.length, ...items);
+    }
+
+    /**
+     * Should return the data (as an object or array) which should be serialized.
+     * Can be overridden by a subclass if it contains other values that should be persisted.
+     *
+     * @return {Object} The data which should be serialized
+     */
+    _serialize() {
+        return this;
+    }
+
+    /**
+     * Is called when the list is unserialized.
+     * This function will repopulate the list with serialized values.
+     * Can be overridden by a subclass if it contains other values that should has been persisted (see _serialize).
+     *
+     * @param  {Object} data The unserialized data.
+     */
+    _unserialize(data) {
+        this.replaceAll(data);
+    }
+
+    /**
+     * Sets a given key to a given value.
+     * Should not really be called outside
+     *
+     * @param {String} prop  The name of the key which is being updated
+     * @param {[type]} value The new value for the given key.
+     */
+    _set(prop, value) {
+        this[prop] = value;
+
+        this._invalidate();
+    }
+
+    /**
+     * Informs all subscribers that the data has been updated
+     */
+    _invalidate() {
+        this.subscribers.forEach((s) => s.invalidate());
+        this.subscribers.forEach((s) => s.handler(this));
+    }
+
+    /**
+     * Returns a string representation of this list.
+     * @return {String} A string representation of this list.
+     */
+    toString() {
+        return JSON.stringify(this._serialize());
+    }
+}
+
+class ListProxy {
+    set(obj, prop, value) {
+        if(typeof obj._set !== "function") {
+            throw new Error(`Method _set(prop, value) is not defined on '${obj.constructor.name}'`);
+        }
+
+        obj._set(prop, value);
+        return true;
+    }
+}
+
+export function observe(list) {
+    return new Proxy(list, new ListProxy());
+}
+
+export function list(items) {
+    return observe(new ObservableList(items));
+}
+
+export function persist(list, name) {
+    let prevData = localStorage.getItem(name);
+    if(prevData != null) {
+        list._unserialize(JSON.parse(prevData));
+    }
+    list.subscribe(value => {
+        localStorage.setItem(name, JSON.stringify(value._serialize()));
+    });
+    return list;
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # ObservableList
-Reactive arrays made easy! Inspired by svelte stores.
+> Lightweight, powerful & framework-agnostic JavaScript state management in less than 1.5KB!
+
+`ObservableList` is a simple, but flexible way of storing data in Single Page Applications, based on the paradigm of *stores*, presented by [Svelte](https://svelte.dev/docs#svelte_store).
+The goal is to make it as simple to work with arrays as with primitive data-types, such as integers, booleans and strings, while still offering reactivity, persistance and sharability between application components.
+
+Also includes helper for syncronising data with LocalStorage.
+
+## Demo
+@TODO
+
+## Installing
+
+The package can be installed with NPM or Yarn.
+
+**Yarn**
+```
+yarn install @jlndk/ObservableList
+```
+
+**NPM**
+```
+npm install @jlndk/ObservableList
+```
+
+## Usage
+See [Wiki](https://github.com/jlndk/ObservableList/wiki/Usage).
+
+**TL;DR:**
+```js
+import {list} from '@jlndk/ObservableList';
+const list = list();
+// Can also be initialized with initial values like:
+// const list = list(["initial", "values"]);
+list.subscribe(updatedList => {
+    //Do what you want with updated list...
+});
+
+//Since ObservableList inherits from Array, all of those methods, such as filter, reduce and push are available.
+```
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## Tests
+@TODO
